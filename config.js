59376c031cec3ec79d1269517e5255618237ac9f
Leo  Antelyes <lantelyes@quuantstamp.com>
2020-02-11 10:13:40 -0800

    Update config.js

--- config.js
+++ config.js
@@ -3,12 +3,12 @@ dotenv.config();
 
 module.exports = {
   ropsten: {
-    rpcOrigin: 'https://ropsten.infura.io',
+    rpcOrigin: `https://ropsten.infura.io/v3/${process.env.INFURA_KEY}`,
     privateKey: process.env.PRIVATE_KEY,
     tokenAddress: process.env.TOKEN_ADDRESS,
     amount: 100,
     decimals: 18,
     gas: 60000,
     maxBalance: 1000
-  },
-}
+  }
+};

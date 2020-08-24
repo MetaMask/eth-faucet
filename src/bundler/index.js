f98f5eca0bf53c0e94e93dace82f4db27712b655
kumavis <aaron@kumavis.me>
2020-08-24 20:45:07 +0800

    lavamoat - start of lavamoat-browserify policy

--- src/bundler/index.js
+++ src/bundler/index.js
@@ -1,6 +1,7 @@
 const fs = require('fs')
 const path = require('path')
-const Browserify = require('browserify')
+const browserify = require('browserify')
+// const lavamoat = require('lavamoat-browserify')
 const envify = require('envify/custom')
 const { address } = require('../server/get-config')
 
@@ -8,13 +9,20 @@ const srcPath = path.join(__dirname, '/../webapp/index.js')
 const destPath = path.join(__dirname, '/../../build/app.js')
 
 // prepare app bundle
-const browserify = Browserify()
+const bundler = browserify(lavamoat.args)
+
 // inject faucet address
-browserify.transform(envify({
+bundler.transform(envify({
   FAUCET_ADDRESS: address
 }))
-browserify.add(srcPath)
+
+// add lavamoat protections
+// (doesnt work under lavamoat-node yet)
+// bundler.plugin(lavamoat)
+
+// add app entry
+bundler.add(srcPath)
 
 // build app
-browserify.bundle()
+bundler.bundle()
   .pipe(fs.createWriteStream(destPath))

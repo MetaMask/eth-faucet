1aad8e16917f264c2f2bc6f3f9cbbb1b0f48b752
kumavis <aaron@kumavis.me>
2021-05-02 17:29:36 +0800

    build - fix build process

--- src/bundler/index.js
+++ src/bundler/index.js
@@ -1,16 +1,18 @@
 const fs = require('fs')
 const path = require('path')
 const browserify = require('browserify')
-// const lavamoat = require('lavamoat-browserify')
+const lavamoat = require('lavamoat-browserify')
 const envify = require('envify/custom')
 const { address } = require('../server/get-config')
 
-const srcPath = path.join(__dirname, '/../webapp/index.js')
-const destPath = path.join(__dirname, '/../../build/app.js')
+const srcDir = path.join(__dirname, '..', 'webapp')
+const htmlPath = path.join(srcDir, 'index.html')
+const entryPath = path.join(srcDir, 'index.js')
+const buildDir = path.join(__dirname, '..', '..', 'build', )
+const destPath = path.join(buildDir, 'app.js')
 
 // prepare app bundle
-// const bundler = browserify(lavamoat.args)
-const bundler = browserify()
+const bundler = browserify(lavamoat.args)
 
 // inject faucet address
 bundler.transform(envify({
@@ -18,13 +20,20 @@ bundler.transform(envify({
 }))
 
 // add lavamoat protections
-// (doesnt work under lavamoat-node yet)
-// bundler.plugin(lavamoat)
+const policyDir = path.join(srcDir, 'lavamoat', 'browserify')
+bundler.plugin(lavamoat, {
+  policy: path.join(policyDir, 'policy.json'),
+  policyOverride: path.join(policyDir, 'policy-override.json'),
+})
 
 // add app entry
-bundler.add(srcPath)
+bundler.add(entryPath)
 
-fs.mkdirSync(path.dirname(destPath), { recursive: true })
+// ensure dest dir exists
+fs.mkdirSync(buildDir, { recursive: true })
+
+// copy html file over
+fs.copyFileSync(htmlPath, path.join(buildDir, 'index.html'))
 
 // build app
 bundler.bundle()
